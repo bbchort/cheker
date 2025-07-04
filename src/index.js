5bb3c5036f9492acce5fb09cfa6a7df4c864ddf0
bbchort <gridchin.pismorf@gmail.com>
2025-07-04 13:17:38 +0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AuctionChecker from "./AuctionChecker";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AuctionChecker />
+  </React.StrictMode>
+);
